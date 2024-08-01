7fadcb7162a06fd324a7172d577d6357e2d6f755
Vito Medlej <vito.medlej@gmail.com>
2024-08-01 11:29:42 +0300

    image sources fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,10 @@ const Home = async () => {
   // DO NOT ask me why the data is nested the way it is
   // I wrote it at 1am in the morning, HALF ASLEEP  
   
+  console.log('ImagesResponse: ', ImagesResponse);
   const categories = categoriesData?.success !== false ? categoriesData?.Categories[0]?.cateArray : null;
-  let imagesArray1 = ImagesResponse.success && ImagesResponse.data && ImagesResponse.data.Images[0].imagesArray.length > 0 ? ImagesResponse.data.Images[0].imagesArray : null;
-  let imagesArray2 = ImagesResponse.success && ImagesResponse.data2 && ImagesResponse.data2.Images2[0].imagesArray.length > 0 ? ImagesResponse.data2.Images2[0].imagesArray : null;
+  let imagesArray1 = ImagesResponse.success && ImagesResponse.data && ImagesResponse.data?.Images?.length > 0 && ImagesResponse.data.Images[0].imagesArray.length > 0 ? ImagesResponse.data.Images[0].imagesArray : null;
+  let imagesArray2 = ImagesResponse.success && ImagesResponse.data2 && ImagesResponse.data?.Images?.length > 0 && ImagesResponse.data2.Images2[0].imagesArray.length > 0 ? ImagesResponse.data2.Images2[0].imagesArray : null;
   return (
     <PreLoader
     // categories={null}
