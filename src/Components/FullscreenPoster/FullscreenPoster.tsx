96d3bf365b61548debb1f26d20bf61a6c6fb62b1
Vito Medlej <vito.medlej@gmail.com>
2024-09-03 13:15:29 +0300

    Explore

--- src/Components/FullscreenPoster/FullscreenPoster.tsx
+++ src/Components/FullscreenPoster/FullscreenPoster.tsx
@@ -2,15 +2,19 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import Btn from '../Btn/Btn'
+import { useRouter } from 'next/navigation'
 
 const Index = ({secondSectionImage} : {secondSectionImage:string}) => {
+  const router = useRouter();
   return (
     <Box className='relative animate-on-scroll' sx={{}}>
         <Box sx={{width:'100%', height:'600px',display:{xs:'flex'}}}>
             <img src={`${secondSectionImage}`} alt="" className="img"  />
         </Box>
         <Box className='absolute center w100 auto flex' sx={{top:'80%',zIndeX:12}}>
-          <Btn>
+          <Btn
+          onClick={()=>router.push('/collection/products')}
+          >
             Explore Collections Now
           </Btn>
         </Box>
